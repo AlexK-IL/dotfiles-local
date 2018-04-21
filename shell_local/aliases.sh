2a1eebd9860a84c41e275a1c805f9373ce3f65ad
Alex Kubica <alexkubicail@gmail.com>
2018-04-21 16:01:21 +0300

    update config

---
+++ shell_local/aliases.sh
@@ -0,0 +1,16 @@
+# Local aliases
+alias pacaur='pacaur --aur' # Run pacaur only with aur
+alias auri='pacaur -y'
+alias paci='sudo pacman -S'
+alias pacrm='sudo pacman -Rns'
+alias pacup='sudo pacman -Syyu'
+alias copy='xclip -selection clipboard -in'
+alias paste='xclip -selection clipboard -out'
+alias torrent='transmission-remote-cli'
+alias mountandroid='simple-mtpfs ~/mnt'
+alias umountandroid='fusermount -u ~/mnt'
+alias bkupkgs='pacman -Qqe | grep -v "$(pacman -Qqm)" > ~/.dotfiles-local/backup/pacman.lst && pacman -Qqm > ~/.dotfiles-local/backup/aur.lst'
+alias cpw='lpass show --password --clip'
+alias i3cheatsheet='egrep ^bind ~/.config/i3/config | cut -d '\'' '\'' -f 2- | sed '\''s/ /\t/'\'' | column -ts $'\''\t'\'' | pr -2 -w 160 -t | less'
+alias trashls='gio list trash://'
+alias trashempty='gio trash --empty'
