7c386159a1a63a814917bd7cb3f7b80effadad23
Alex Kubica <alexkubicail@gmail.com>
2018-05-25 20:23:41 +0300

    set keyboard to english on lock screen

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-# TODO Make sure keyboard is english
+# Make sure keyboard is english
+setxkbmap -layout us
 
 # Take a screenshot and blur it
 scrot /tmp/screen.png  
@@ -11,3 +12,6 @@ i3lock -i /tmp/screen.png
 
 # Remove screenshot
 rm /tmp/screen.png  
+
+# Return keyboard layout
+setxkbmap -layout us,il -option grp:alt_shift_toggle
