b08619ec614d3d9ff961873aab9c080406c66e36
Alex Kubica <alexkubicail@gmail.com>
2018-05-27 16:31:15 +0300

    update lock script

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
-i3lock-fancy
+# Make sure keyboard is english
+setxkbmap -layout us
+
+# Take a screenshot and blur it
+scrot /tmp/screen.png  
+convert /tmp/screen.png -blur 0x8 /tmp/screen.png 
+
+# Lock the screen with the blured screenshot
+i3lock -i /tmp/screen.png  
+
+# Remove screenshot
+rm /tmp/screen.png  
+
+# Return keyboard layout
+setxkbmap -layout us,il -option grp:alt_shift_toggle
