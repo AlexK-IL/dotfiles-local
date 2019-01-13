a2c80626d71c7b2ebf3c92fc326e113856f5ae6f
Alex Kubica <alexkubicail@gmail.com>
2019-01-13 14:19:59 +0200

    update

--- bin/t3connect.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo -e "power on\nagent on\nconnect 17:02:06:E7:08:43" | bluetoothctl
--- shell_local/aliases.sh
+++ shell_local/aliases.sh
@@ -1,2 +1,1 @@
 # Local aliases
-alias wifirestart='sudo systemctl restart dhcpcd@wlp3s0.service'
